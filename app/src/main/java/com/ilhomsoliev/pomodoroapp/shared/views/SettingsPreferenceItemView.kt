0b6148ca2c332169b666d6fb5a980f273ce2291a
Ilhom Soliev <soliev.ilhom.2005@gmail.com>
2023-10-18 17:31:41 +0300

    update

--- app/src/main/java/com/ilhomsoliev/pomodoroapp/shared/views/SettingsPreferenceItemView.kt
+++ app/src/main/java/com/ilhomsoliev/pomodoroapp/shared/views/SettingsPreferenceItemView.kt
@@ -3,9 +3,11 @@ package com.ilhomsoliev.pomodoroapp.shared.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import com.ilhomsoliev.pomodoroapp.databinding.SettingsPreferenceItemViewBinding
 
 class SettingsPreferenceItemView @JvmOverloads constructor(context: Context, attrs: AttributeSet?) :
     View(context, attrs) {
 
-    private val binding:
+    // private val binding: SettingsPreferenceItemViewBinding
+
 }
